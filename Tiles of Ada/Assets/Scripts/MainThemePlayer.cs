1675bb76ec2191f3894d4432eb0f31296849ec12
Katie Jahanmir <katietj@gmail.com>
2019-01-14 14:08:18 -0800

    added music to levels

---
+++ Tiles of Ada/Assets/Scripts/MainThemePlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainThemePlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+        SetUpSingleton();
+	}
+
+    private void SetUpSingleton()
+    {
+        if(FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 4 || currentSceneIndex == 5)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
